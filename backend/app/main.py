2199b2106802fa8c2a2b2ca75309a0a6801a81f5
Nonso dev <thewarenerd@gmail.com>
2025-07-20 10:27:58 +0100

    fixed cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,9 +36,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
-        "https://*.vercel.app",
-        "https://cancerguard-ai-frontend.vercel.app",
-        "https://ac7ab8534ec8.ngrok-free.app"
+        "https://cancerguard-ai.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
